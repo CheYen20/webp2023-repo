79cf674acaf0fcf8da4d18a794e0b0dd5f907375
CheYen20 <B0844102@cgu.edu.tw>
2023-06-05 20:07:47 +0800

    push

---
+++ FinalProject/src/components/About/About.js
@@ -0,0 +1,42 @@
+const About = () => {
+  return (
+<div className="about_me section clearfix" id="about">
+<div className="container">
+        <h2>about me</h2>
+        <div className="summary">
+          <div className="col-left">
+            <div className="avatar">
+              <img src="https://img.onl/DNIJN" alt="avatar" />
+            </div>
+          </div>
+
+          <div className="col-right">
+            <div className="personal-info">
+              <p>我是張哲嚴，大家都叫我CheYen或是CY；2023年於長庚大學資訊管理學系畢業，對撰寫程式有興趣。</p>
+              <br />
+              <p>【高中時期】</p>
+              <p>高中曾擔任服務性社團副社長、迎新活動副召。</p>
+              <br />
+              <p>【大學時期】</p>
+              <p>大學時期也參與了系羽、竹友會、烘焙社，以及返鄉服務隊等大型活動。</p>
+              <br />
+              <p>【興趣與嗜好】</p>
+              <p>我的興趣是聽各類型的Podcast, e.g.台灣通、英文不難；另外，我平日會利用閒暇時間慢跑2~3次，保持身體處於最佳狀態。</p>
+              <p>最近養成早睡早起睡滿七小時的好習慣。</p>
+              <br />
+              <p>【家庭關係】</p>
+              <p>我生長於一個完整、健全的家庭，使我能夠無憂無慮地長大，並能夠追求自己的理想。</p>
+            </div>
+
+            <div className="button-group">
+              <div className="download_cv">
+                <a href="https://www.instagram.com/cy_3952/" target='_blank' rel='noopener noreferrer'>instagram</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+  )
+}
+export default About
